b3ebe25dc256ec4be8831b7ed8010f28d5d3c516
unknown <leih202205@gmail.com>
2024-04-26 15:55:39 -0700

    ready to deploy backend

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,13 +1,9 @@
-import dotenv from 'dotenv';
-dotenv.config()
+
 let allowedOrigins;
-if (process.env.NODE_ENV=="development"){
+
 allowedOrigins = [
-    'http://localhost:5000',
-    'http://localhost:8000',
-    'http://localhost:3000',
+    'http://expenses.onrender.com',
     ]
-} 
 
 
 export {allowedOrigins}
